cccb26b2611d333a8d4d07c93c21a2ac518522c5
Fred Lock <flock@idauto.net>
2021-12-15 10:39:12 -0700

    master: Added code to respond to an F10 keypress on the home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,28 @@
 ﻿import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({
+    templateUrl: 'home.component.html'
+})
 export class HomeComponent {
     user: User;
 
     constructor(private accountService: AccountService) {
         this.user = this.accountService.userValue;
+    }
+
+    @HostListener('document:keydown', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if (event.key == 'F10') {
+            event.preventDefault();
+            this.accountService.logout();
+        }
+    }
+
+    logout() {
+        this.accountService.logout();
     }
 }
